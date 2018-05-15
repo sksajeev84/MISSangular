30b40d2ecdb5c31273ac5ab71f8abdcda1589af2
sksajeev84 <32258971+sksajeev84@users.noreply.github.com>
2018-05-15 01:01:24 -0700

    MISS

---
+++ src/app/my-new-interface.ts
@@ -0,0 +1,12 @@
+export interface MyNewInterface {
+   
+	equipmentId:string;
+	equipmentname:string;
+	availbilitystatus:string;
+	equipmentdescription:string;
+	equipmentLatitude:any;
+	equipmentLongitude:any;
+	equipmentlocation:string;
+	connectivitystatus:string;
+	assetTypeId:string;
+}
